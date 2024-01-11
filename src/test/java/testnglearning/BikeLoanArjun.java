79ef7104b9d96804280d4c3373fd0f0e0de51772
Sam Smith <sam@example.com>
2024-01-11 11:10:05 +0530

    this is the second commit

--- src/test/java/testnglearning/BikeLoanArjun.java
+++ src/test/java/testnglearning/BikeLoanArjun.java
@@ -8,6 +8,21 @@ public class BikeLoanArjun {
 @Test	
 public void ArjunLoan() {
 	System.out.println("Bike Loadn Arjun");
+	System.out.println("Before leaving");
+
+}
+
+@Test	
+public void ArjunBikeLoan() {
+	System.out.println("Bike Loadn Arjun");
+	System.out.println("Before leaving");
+
+}
+
+@Test	
+public void ArjunHomeLoan() {
+	System.out.println("Bike Loadn Arjun");
+	System.out.println("Before leaving");
 
 }
 }
